e8ef235cec8d57406758192517fba96fec2af657
PDSO <herlinasunaryanto@Sunar-MacBook-Pro.local>
2025-05-14 19:51:28 +0700

    feat: revamp ui

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,27 @@
+import { RemotePattern } from "next/dist/shared/lib/image-config"
+
+const RemotePatterns: Array<URL | RemotePattern> = [
+  {
+    protocol: 'https',
+    hostname: 'cdn.simpleicons.org',
+    pathname: '/**',
+    port: ''
+  },
+  {
+    protocol: 'https', 
+    hostname: 'img.shields.io',
+    pathname: '/**',
+    port: ''
+  }
+]
+
 const Settings = {
   Cookies: {
     Theme: "anantara-theme",
     Language: "anantara-language"
   },
   Images: {
-    RemotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.simpleicons.org',
-        pathname: '/**',
-        port: ''
-      },
-      {
-        protocol: 'https', 
-        hostname: 'img.shields.io',
-        pathname: '/**',
-        port: ''
-      }
-    ]
+    RemotePatterns: RemotePatterns
   }
 }
 
